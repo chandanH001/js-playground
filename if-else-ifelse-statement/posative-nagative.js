cd08441383327a1f755d18b3ede039673b8146d8
Chandan <chandan.halder96@gmail.com>
2025-05-20 14:23:49 +0600

    if else for find out Nagative Posative

---
+++ if-else-ifelse-statement/posative-nagative.js
@@ -0,0 +1,11 @@
+// Positive , negative, or zero solutions 
+
+let number = -4;
+if(number > 0){
+console.log('this number is Posative');
+
+} else if (number < 0){
+    console.log('This number is Nagative'); 
+}else {
+    console.log('Opps Its Zero');
+}
