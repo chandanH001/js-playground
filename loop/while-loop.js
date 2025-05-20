a625fb1f91f68fd340f1a121e5a5f67c371b40f4
Chandan <chandan.halder96@gmail.com>
2025-05-20 20:12:10 +0600

    while loop

---
+++ loop/while-loop.js
@@ -0,0 +1,16 @@
+
+let num = 1 ;
+/*
+while(num <= 80){
+console.log('akash' ,  num);
+num ++;
+}
+*/
+// even number print from 1 to 80
+
+while(num <= 80){
+    if(num % 2 == 0){
+    console.log(num);
+    }
+    num++ ;
+}
