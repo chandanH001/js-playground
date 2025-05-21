314062a94e1705137aed737799cb95e59c498b43
Chandan <chandan.halder96@gmail.com>
2025-05-21 13:21:34 +0600

    2nd example of ibject

--- object/object.js
+++ object/object.js
@@ -3,8 +3,16 @@ name: "chandan haldar",
 id: 25,
 age: 30,
 section: "c"
-}
+};
 student.nick = "cd"
 console.log(student);
 // console.log(student.nick);
 
+let product = {
+name : "Mango",
+From : "khulna",
+Price: 250 ,
+Unit: 20
+};
+
+console.log(product);
