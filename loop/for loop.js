35286bc9bca59975e833180bee60ab9cd9d7639f
Chandan <chandan.halder96@gmail.com>
2025-05-22 13:54:23 +0600

    for loop

---
+++ loop/for loop.js
@@ -0,0 +1,17 @@
+
+// 0 to 10 even number 
+for(let i=0; i<=10; i=i+2){
+    console.log(i);
+
+}
+
+// 200 to 250 total sum 
+let sum = 0;
+for (let i=200; i<=250; i++){
+
+    // console.log(i);
+
+    sum = sum + i;
+    
+}
+console.log(sum);
